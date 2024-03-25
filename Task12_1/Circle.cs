3e5a65965469b905f78735199f2b699715b80e48
Svetlana Jourkova <belkina_s@mail.ru>
2024-03-26 00:17:44 +0300

    Добавьте файлы проекта.

---
+++ Task12_1/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12_1
+{
+    static class Circle
+    {
+        static public double GetLenthCircle(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquareCircle(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public string InCircle(int x, int y, int r, int x0, int y0)
+        {
+            if ((x - x0) * (x - x0) + (y - y0) * (y - y0) <= r * r)
+                return $"точка ({x}, {y}) принадлежит кругу радиусом {r}";
+            return $"точка ({x}, {y}) не принадлежит кругу радиусом {r}";
+        }
+    }
+}
---
+++ Task12_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task12_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату точки x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату точки y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату центра x0: ");
+            int x0 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату центра y: ");            
+            int y0 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            double lenth = Circle.GetLenthCircle(r);
+            double square = Circle.GetSquareCircle(r);
+            Console.WriteLine($"длина окружности = {square:.00} \nплощадь окружности = {square:.00}");
+            Console.WriteLine(Circle.InCircle(x, y, r, x0, y0));
+
+        }
+    }
+}
